09936b2fcb9eb5e5f769d383c7b5d2f465e415a3
D Hill <daaaniel.hill@gmail.com>
2025-11-04 17:52:11 +0200

    add Contestant entity

---
+++ SecretSantaApp/Entities/Contestant.cs
@@ -0,0 +1,21 @@
+﻿namespace SecretSantaApp.Entities;
+
+public class Contestant : IContestant
+{
+    public string Id { get; set; }
+    
+    public string Username { get; set; }
+    
+    public string SpeciesMain { get; set; }
+    public string[] SpeciesOptional { get; set; }
+    
+    public string Size { get; set; }
+    public string Background { get; set; }
+    public string[] Characters { get; set; }
+    
+    public Contestant? Pair {get; set;}
+    
+    public static string UsedSpeciesCategory { get; set; }
+    public static string UsedSizeCategory { get; set; }
+    public static string UsedBackgroundCategory { get; set; }
+}
---
+++ SecretSantaApp/Interfaces/IContestant.cs
@@ -0,0 +1,13 @@
+﻿namespace SecretSantaApp
+{
+    public interface IContestant 
+    {
+        string Id { get; set; }
+        string Username { get; set; }
+        string SpeciesMain { get; set; }
+        string[] SpeciesOptional { get; set; }
+        string Size { get; set; }
+        string Background { get; set; }
+        string[] Characters { get; set; }
+    }
+}
